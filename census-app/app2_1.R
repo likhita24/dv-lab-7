925580d2ca3e31d75f45526c285bb58be60e5c12
Sri Likhita Bitra <bitra.1@iitj.ac.in>
2023-04-11 17:40:45 +0530

    Add files via upload

---
+++ census-app/app2_1.R
@@ -0,0 +1,31 @@
+library(shiny)
+
+# Define UI ----
+ui <- fluidPage(
+  titlePanel("title panel"),
+  
+  sidebarLayout(
+    sidebarPanel("sidebar panel"),
+    mainPanel("main panel")
+  ),
+  sidebarLayout(position = "right",
+                sidebarPanel("sidebar panel"),
+                mainPanel("main panel")
+  ),
+  mainPanel(
+    h1("First level title"),
+    h2("Second level title"),
+    h3("Third level title"),
+    h4("Fourth level title"),
+    h5("Fifth level title"),
+    h6("Sixth level title")
+  )
+)
+
+# Define server logic ----
+server <- function(input, output) {
+  
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
